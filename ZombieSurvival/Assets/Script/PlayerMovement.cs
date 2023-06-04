3deb2d58c17738fb0572af0880c2aa207b76b97a
Insector3307 <rnlarsen25@gmail.com>
2023-06-03 17:36:36 -0700

    Starting Player
    We walkin

---
+++ ZombieSurvival/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rigidBody;
+    [SerializeField] float speed;
+
+    [SerializeField] Camera cam;
+    Animator anime;
+    Vector3 mouse;
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        anime = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        mouse = cam.ScreenToWorldPoint(Input.mousePosition);
+        float angle = Mathf.Atan2(mouse.y - transform.position.y, mouse.x - transform.position.x) * Mathf.Rad2Deg;
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, angle);
+
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            rigidBody.velocity = new Vector2(0f, speed * Mathf.Sign(Input.GetAxis("Vertical")));
+            anime.SetBool("idle", false);
+            anime.SetBool("holdingGun", true);
+        }
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            rigidBody.velocity = new Vector2(speed * Mathf.Sign(Input.GetAxis("Horizontal")), 0f);
+        }
+
+        if (Input.GetAxis("Horizontal") + Input.GetAxis("Vertical") == 0 && !anime.GetBool("idle"))
+        {
+            anime.SetBool("idle", true);
+        }
+    }
+}
