68075fa2eb7452ee56e687e917b3a8f4593404b7
AnselmoNoriega <anselmonoriega3@gmail.com>
2023-06-09 22:04:53 -0700

    PlayerInputSystem

---
+++ ZombieSurvival/Assets/InputSystem/Inputs.cs
@@ -0,0 +1,176 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/InputSystem/Inputs.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Inputs : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Inputs()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Inputs"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""58a26bbc-c4c2-433f-8f8f-257c3e9b6393"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""5e25c508-3abb-4754-b176-2d7a70ae8f9e"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""1fc153c5-70f8-4dda-82d6-64b5ed6da0c1"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""558c4a73-1104-4bb9-b916-e1bc281cefcc"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""9436214b-fea8-445a-b14e-0144a5e8097d"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""74e5387d-9aee-4d96-8ef6-6e2456eaf68c"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""c18ea34e-a199-495d-a971-d6ff236dc519"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Movement = m_Player.FindAction("Movement", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Movement;
+    public struct PlayerActions
+    {
+        private @Inputs m_Wrapper;
+        public PlayerActions(@Inputs wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_Player_Movement;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+    }
+}
--- ZombieSurvival/Assets/Script/PlayerMovement.cs
+++ ZombieSurvival/Assets/Script/PlayerMovement.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rigidBody;
+    [SerializeField] private Vector2 _movement;
     [SerializeField] float speed;
 
     [SerializeField] Camera cam;
@@ -13,35 +16,63 @@ public class PlayerMovement : MonoBehaviour
     Animator anime;
     Vector3 mouse;
 
+    private Inputs _input;
+
+    private void Awake()
+    {
+        _input = new Inputs();
+    }
+
+    private void OnEnable()
+    {
+        _input.Enable();
+        _input.Player.Movement.performed += Movement;
+        _input.Player.Movement.canceled += StopMovement;
+    }
+
     void Start()
     {
+        _movement = Vector2.zero;
         rigidBody = GetComponent<Rigidbody2D>();
         anime = GetComponent<Animator>();
     }
 
+    private void FixedUpdate()
+    {
+        rigidBody.velocity = _movement * speed;
+    }
+
     void Update()
     {
-        mouse = cam.ScreenToWorldPoint(Input.mousePosition);
+        mouse = cam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         float angle = Mathf.Atan2(mouse.y - transform.position.y, mouse.x - transform.position.x) * Mathf.Rad2Deg;
         transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, angle);
 
-        if (Input.GetAxis("Vertical") != 0)
-        {
-            rigidBody.velocity = new Vector2(0f, speed * Mathf.Sign(Input.GetAxis("Vertical")));
-            anime.SetBool("idle", false);
-            anime.SetBool("holdingGun", true);
-        }
+       SetAnimStates();
+    }
 
-        if (Input.GetAxis("Horizontal") != 0)
-        {
-            rigidBody.velocity = new Vector2(speed * Mathf.Sign(Input.GetAxis("Horizontal")), 0f);
-        }
-
-        if (Input.GetAxis("Horizontal") + Input.GetAxis("Vertical") == 0 && !anime.GetBool("idle"))
+    private void SetAnimStates()
+    {
+        if(_movement.magnitude == 0)
         {
             anime.SetBool("idle", true);
         }
+        else
+        {
+            anime.SetBool("idle", false);
+        }
     }
+
+    private void Movement(InputAction.CallbackContext value)
+    {
+        _movement = value.ReadValue<Vector2>();
+    }
+
+    private void StopMovement(InputAction.CallbackContext value)
+    {
+        _movement = Vector2.zero;
+    }
+
     public void DisplayPrompt()
     {
         prompt.gameObject.SetActive(true);
@@ -50,4 +81,35 @@ public class PlayerMovement : MonoBehaviour
     {
         prompt.gameObject.SetActive(false);
     }
+
+    private void OnDisable()
+    {
+        _input.Disable();
+        _input.Player.Movement.performed -= Movement;
+        _input.Player.Movement.canceled -= StopMovement;
+    }
 }
+
+//Tagged
+//{
+//    mouse = cam.ScreenToWorldPoint(Input.mousePosition);
+//    float angle = Mathf.Atan2(mouse.y - transform.position.y, mouse.x - transform.position.x) * Mathf.Rad2Deg;
+//    transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, angle);
+
+//    if (Input.GetAxis("Vertical") != 0)
+//    {
+//        rigidBody.velocity = new Vector2(0f, speed * Mathf.Sign(Input.GetAxis("Vertical")));
+//        anime.SetBool("idle", false);
+//        anime.SetBool("holdingGun", true);
+//    }
+
+//    if (Input.GetAxis("Horizontal") != 0)
+//    {
+//        rigidBody.velocity = new Vector2(speed * Mathf.Sign(Input.GetAxis("Horizontal")), 0f);
+//    }
+
+//    if (Input.GetAxis("Horizontal") + Input.GetAxis("Vertical") == 0 && !anime.GetBool("idle"))
+//    {
+//        anime.SetBool("idle", true);
+//    }
+//}
