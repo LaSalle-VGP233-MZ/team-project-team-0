8d7171d51b716de4b722a84e3e050e6bbf6b7214
AnselmoNoriega <anselmonoriega3@gmail.com>
2023-06-16 20:52:30 -0700

    Shooting Independent script

--- ZombieSurvival/Assets/Script/Player/PlayerMovement.cs
+++ ZombieSurvival/Assets/Script/Player/PlayerMovement.cs
@@ -14,31 +14,16 @@ public enum GunType
 }
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody2D rigidBody;
     [SerializeField] private Vector2 _movement;
     [SerializeField] float speed;
 
     [SerializeField] Camera cam;
     [SerializeField] private Transform prompt;
 
-    [SerializeField] private ParticleSystem gunShotLight;
+    private Rigidbody2D rigidBody;
 
     Animator anime;
     Vector3 mouse;
-
-    //Gun Damage / Gun Range / Gun Ammo / Rate of Fire
-    private Vector4[] statBlocks = { new Vector4(5f, 8f, 10, 0.2f), new Vector4(10f, 14f, 10, 0.05f), new Vector4(20f, 5f, 10, 0.75f) };
-    private float timeTilFire = 0f;
-
-    [SerializeField] private GunType currentGun = GunType.Pistol;
-    [SerializeField] private TextMeshProUGUI ammoDisplay;
-    [SerializeField] private TextMeshProUGUI pointsDisplay;
-
-    [SerializeField] private Image ammoIcon;
-
-    [SerializeField] private Sprite[] bulletIcons;
-    [SerializeField] private AudioClip[] bulletNoise;
-    [SerializeField] private AudioClip empty;
 
     private Inputs _input;
     private bool _inTrigger;
@@ -46,8 +31,6 @@ public class PlayerMovement : MonoBehaviour
     private float barricadeHoldTimer = 2f;
     private float barricadeHoldCount = 0;
     private bool isBarricadeHolding = false;
-
-    private int points = 0;
 
 
     private void Awake()
@@ -62,8 +45,6 @@ public class PlayerMovement : MonoBehaviour
         _input.Player.Movement.canceled += StopMovement;
         _input.Actions.InputPressedBar.performed += InputPressedF;
         _input.Actions.InputPressedBar.canceled += InputLeaveF;
-        _input.Actions.InputPressedFire.performed += PressedFire;
-        _input.Actions.InputPressedFire.canceled += ReleasedFire;
     }
 
     void Start()
@@ -72,9 +53,6 @@ public class PlayerMovement : MonoBehaviour
         _movement = Vector2.zero;
         rigidBody = GetComponent<Rigidbody2D>();
         anime = GetComponent<Animator>();
-
-        ammoDisplay.text = statBlocks[(int)currentGun].z.ToString();
-        ammoIcon.sprite = bulletIcons[(int)currentGun];
     }
 
     private void FixedUpdate()
@@ -85,19 +63,16 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         mouse = cam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
         float angle = Mathf.Atan2(mouse.y - transform.position.y, mouse.x - transform.position.x) * Mathf.Rad2Deg;
+
         transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, angle);
 
-        if (timeTilFire > -1)
-            timeTilFire -= Time.deltaTime;
 
         SetAnimStates();
 
         TimerBarricade();
 
-        ammoDisplay.text = statBlocks[(int)currentGun].z.ToString();
-        ammoIcon.sprite = bulletIcons[(int)currentGun];
-        pointsDisplay.text = "Points: " + points.ToString();
         prompt.transform.rotation = Quaternion.identity;
         prompt.transform.position = new Vector3(transform.position.x,transform.position.y + 1,0);
     }
@@ -140,66 +115,25 @@ public class PlayerMovement : MonoBehaviour
         _input.Player.Movement.canceled -= StopMovement;
         _input.Actions.InputPressedBar.performed -= InputPressedF; 
         _input.Actions.InputPressedBar.canceled -= InputLeaveF;
-        _input.Actions.InputPressedFire.performed -= PressedFire;
-        _input.Actions.InputPressedFire.canceled -= ReleasedFire;
     }
 
     public void InputPressedF(InputAction.CallbackContext value)
     {
-        if (_inTrigger && interactObject.CompareTag("Breakable"))
-        {
-            isBarricadeHolding = true;
-        }
-        else if (_inTrigger && interactObject.CompareTag("Gun"))
-        {
-            PurchaseGun(interactObject);
-            _inTrigger = false;
-        }
+        //if (_inTrigger && interactObject.CompareTag("Breakable"))
+        //{
+        //    isBarricadeHolding = true;
+        //}
+        //else if (_inTrigger && interactObject.CompareTag("Gun"))
+        //{
+        //    PurchaseGun(interactObject);
+        //    _inTrigger = false;
+        //}
     }
 
     private void InputLeaveF(InputAction.CallbackContext value)
     {
         barricadeHoldCount = 0;
         isBarricadeHolding = false;
-    }
-
-    public void PressedFire(InputAction.CallbackContext value)
-    {
-        if (timeTilFire <= 0 && statBlocks[(int)currentGun].z > 0)
-        {
-            gunShotLight.Play();
-
-            Vector3 mouseDir = (mouse - transform.position).normalized * statBlocks[(int)currentGun].y;
-            mouseDir.z = 0;
-            Debug.DrawRay(transform.position, mouseDir, Color.red, 0.3f);
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, mouseDir, statBlocks[(int)currentGun].y * 2, LayerMask.GetMask("Map", "Zombies"));
-
-            if (hit.collider != null)
-            {
-                if (hit.collider.gameObject.CompareTag("Zombie"))
-                {
-                    hit.collider.GetComponent<Health>().ReduceHealth(statBlocks[(int)currentGun].x);
-                    hit.collider.GetComponent<ParticleSystem>().Play();
-                    points += 2*(int)statBlocks[(int)currentGun].x;
-                }
-            }
-
-            statBlocks[(int)currentGun].z--;
-            AudioSource.PlayClipAtPoint(bulletNoise[(int)currentGun], new Vector3(0, 0, 0));
-            timeTilFire = statBlocks[(int)currentGun].w;
-        }
-        else if (statBlocks[(int)currentGun].z <= 0)
-        {
-            AudioSource.PlayClipAtPoint(empty, new Vector3(0, 0, 0));
-            timeTilFire = statBlocks[(int)currentGun].w;
-        }
-
-        
-    }
-
-    public void ReleasedFire(InputAction.CallbackContext value)
-    {
-
     }
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -250,21 +184,21 @@ public class PlayerMovement : MonoBehaviour
             isBarricadeHolding = false;
         }
     }
-    public void PurchaseGun(GameObject gunObject)
-    {
-        int price = gunObject.GetComponent<GunPurchase>().price;
-        GunType newGun = gunObject.GetComponent<GunPurchase>().gunType;
-        if (price <= points)
-        {
-            if (newGun == currentGun)
-            {
-                statBlocks[(int)currentGun].z = 10;
-            }
-            else
-            {
-                currentGun = newGun;
-            }
-            points -= price;
-        }
-    }
+    //public void PurchaseGun(GameObject gunObject)
+    //{
+    //    int price = gunObject.GetComponent<GunPurchase>().price;
+    //    GunType newGun = gunObject.GetComponent<GunPurchase>().gunType;
+    //    if (price <= points)
+    //    {
+    //        if (newGun == currentGun)
+    //        {
+    //            statBlocks[(int)currentGun].z = 10;
+    //        }
+    //        else
+    //        {
+    //            currentGun = newGun;
+    //        }
+    //        points -= price;
+    //    }
+    //}
 }
---
+++ ZombieSurvival/Assets/Script/Player/Shooting.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class Shooting : MonoBehaviour
+{
+    [SerializeField]
+    private InputActionReference _input;
+    private Vector3 mouse;
+    private int points;
+
+    private Vector4[] statBlocks = { new Vector4(5f, 8f, 10, 0.2f), new Vector4(10f, 14f, 10, 0.05f), new Vector4(20f, 5f, 10, 0.75f) };
+
+    //Gun Damage / Gun Range / Gun Ammo / Rate of Fire
+    private float timeTilFire;
+    [Header("Camera")]
+    [SerializeField] Camera cam;
+
+    [Header("Gun's Variables")]
+    [SerializeField] private GunType currentGun = GunType.Pistol;
+    [SerializeField] private TextMeshProUGUI ammoDisplay;
+    [SerializeField] private TextMeshProUGUI pointsDisplay;
+    [SerializeField] private Image ammoIcon;
+    [SerializeField] private AudioClip empty;
+    [SerializeField] private ParticleSystem gunShotLight;
+    [SerializeField] private Sprite[] bulletIcons;
+    [SerializeField] private AudioClip[] bulletNoise;
+
+
+    private void OnEnable()
+    {
+        _input.action.Enable();
+        _input.action.performed += PressedFire;
+    }
+
+    void Start()
+    {
+        timeTilFire = 0.0f;
+        points = 0;
+        ammoDisplay.text = statBlocks[(int)currentGun].z.ToString();
+        ammoIcon.sprite = bulletIcons[(int)currentGun];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouse = cam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+        if (timeTilFire > -1)
+        {
+            timeTilFire -= Time.deltaTime;
+        }
+
+        ammoDisplay.text = statBlocks[(int)currentGun].z.ToString();
+        ammoIcon.sprite = bulletIcons[(int)currentGun];
+        pointsDisplay.text = "Points: " + points.ToString();
+    }
+
+    private void OnDisable()
+    {
+        _input.action.Disable();
+        _input.action.performed -= PressedFire;
+    }
+
+    public void PressedFire(InputAction.CallbackContext value)
+    {
+        Debug.Log("idddddddddd");
+        if (timeTilFire <= 0 && statBlocks[(int)currentGun].z > 0)
+        {
+            gunShotLight.Play();
+
+            Vector3 mouseDir = (mouse - transform.position).normalized * statBlocks[(int)currentGun].y;
+            mouseDir.z = 0;
+
+            Debug.DrawRay(transform.position, mouseDir, Color.red, 0.3f);
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, mouseDir, statBlocks[(int)currentGun].y * 2, LayerMask.GetMask("Map", "Zombies"));
+
+            if (hit.collider != null)
+            {
+                if (hit.collider.gameObject.CompareTag("Zombie"))
+                {
+                    hit.collider.GetComponent<Health>().ReduceHealth(statBlocks[(int)currentGun].x);
+                    hit.collider.GetComponent<ParticleSystem>().Play();
+                    points += 2 * (int)statBlocks[(int)currentGun].x;
+                }
+            }
+
+            statBlocks[(int)currentGun].z--;
+            AudioSource.PlayClipAtPoint(bulletNoise[(int)currentGun], new Vector3(0, 0, 0));
+            timeTilFire = statBlocks[(int)currentGun].w;
+
+        }
+        else if (statBlocks[(int)currentGun].z <= 0)
+        {
+            AudioSource.PlayClipAtPoint(empty, new Vector3(0, 0, 0));
+            timeTilFire = statBlocks[(int)currentGun].w;
+        }
+
+
+    }
+
+}
